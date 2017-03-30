251424f2afecd8459163ca22af5e23f6c3a8af4e
Ivan Pereira <ivan@zivan.org>
2017-03-30 14:40:36 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '0.1'
+setup(
+    name='genieorm',
+    version=version,
+    description='Genie ORM for Humans.',
+    long_description="readme",
+    author='Ivan Pereira',
+    author_email='ivan@zivan.org',
+    packages=['genieorm'],
+    zip_safe=False,
+    license='Apache 2.0',
+)
